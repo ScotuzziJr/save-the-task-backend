476956ebb9a6cedcaaef42078f66e8cdb39c1f17
ScotuzziJr <scotuzzijunior@gmail.com>
2022-05-31 10:14:18 -0300

    chore: add aplication properties

--- tasksv/src/main/java/com/tasksv/tasksv/repo/CategoriaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tasksv.tasksv.repo;
-import ch.qos.logback.core.net.server.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepo implements JpaRepository<Categoria, Long>
-{
-    Categoria findById(String id);
-    Categoria findByNome(String nome);
-}
--- tasksv/src/main/java/com/tasksv/tasksv/repo/TarefaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tasksv.tasksv.repo;
-import ch.qos.logback.core.net.server.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepo extends JpaRepository<Tarefa, Long>
-{
-    Tarefa findById(String id);
-    Tarefa findByTitulo(String titulo);
-}
